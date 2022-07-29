818afd452997059911233a35494219cb661ec87c
Kha Nguyen <kevin.nguyenhoang91@gmail.com>
2022-07-29 14:29:18 +0700

    Bump to version 1.49.3-OM

--- version/build.go
+++ version/build.go
@@ -1,7 +1,7 @@
 package version
 
 // VERSION is the main CLI version number. It's defined at build time using -ldflags
-var VERSION = "0.0.0-development"
+var VERSION = "1.49.3-OM"
 
 // BuildNumber is the CI build number that creates the release. It's defined at build time using -ldflags
 var BuildNumber = ""
